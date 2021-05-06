070e647e10196d91095be2538f7e9976787d1717
Bia Polk <bianca@shipwell.com>
2021-05-06 16:34:13 -0500

    Added tamagotchi.py file

--- tamagotchi.py
+++ tamagotchi.py
@@ -8,13 +8,16 @@ class Pet:
     hunger_decrement = 6
     boredom_threshold = 5
     hunger_threshold = 10
-    sounds = ['Wolf']
+    sounds = ['Woof']
+    type = "mammal"
 
-    #Method called upon when creating an instance of a class
+    # Method called upon when creating an instance of a class
+    # In this instance
     def __init__(self,name = 'Dog', age = 0):
         # Attributes
         # We take in the argument
         # Assign it using self.attribute name
+        print("PET CREATED")
         self.name = name
         self.age = age
         self.hunger = randrange(self.hunger_threshold)
@@ -44,13 +47,15 @@ class Pet:
         return state
 
     # OPERATIONS/Actions ----> Methods
+
+    # def what_am_i(self):
+    #     print("I am a {}".format(type))
+
+
     def hi(self):
         print(self.sounds[randrange(len(self.sounds))])
         print("My name is {} ".format(self.name) + " and right now I am " + self.mood() + ".")
         self.reduce_boredom()
-
-    def bark(self):
-         print("WOOF!")
 
     def sleep(self):
         print("WOOF! My name is {}".format(self.name))
@@ -62,8 +67,33 @@ class Pet:
         self.boredom = max(0, self.boredom - self.boredom_decrement)
 
 
+class Dog(Pet):
+    def __init__(self):
+        Pet.__init__(self)
+        print("Dog Created")
+    def bark(self):
+         print(self.sounds)
+    def who_am_i(self):
+        print("I am a dog!")
+
+
+# class Cat:
+#     def __init__(self):
+#         Pet.__init__(self)
+#         print("Cat Created")
+#
+#     def bark(self):
+#         print(self.sounds)
+#
+#     def who_am_i(self):
+#         print("I am a dog!")
+
+
 if __name__ == "__main__":
-    my_pet = Pet('Nea', 8)
-    my_pet.hi()
+    my_pet = Dog()
+    # my_pet = Cat()
+
+    my_pet = Dog()
     my_pet.bark()
+    my_pet.hi()
 
